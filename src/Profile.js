7bb18cc56cd44c301eea78e1353efb2e00dc9e66
harshil-pahuja <harshilpahuja1987@gmail.com>
2025-01-25 16:43:57 -0500

    Profile routing

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {useAuth0} from '@auth0/auth0-react';
+import LogOut from './LogOut.js';
+
 // Used the following source: https://www.geeksforgeeks.org/how-to-build-a-react-app-with-user-authentication/
 const Profile = () => {
     const {user} = useAuth0();
@@ -10,7 +12,13 @@ const Profile = () => {
                     Name: {user.name}</p>
                 <p className="userInfo" id="userInfo2">
                     Email: {user.email}</p>
+                <img
+                    src={user.picture}
+                    alt="User Profile"
+                    className="profile-picture"
+                />
             </div>
+            <LogOut />
         </>
     )
 }
