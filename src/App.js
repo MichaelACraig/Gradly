164377fa32ac66a45d07dc0dedc37ce4c4f350c0
Bhaskar Gnanasakthi <bdg10@Bhaskars-MacBook-Air.local>
2025-01-25 17:30:24 -0500

    Profile page error

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import house from './house.jpg';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 import {useAuth0} from '@auth0/auth0-react';
-import Login from './Login.js';
 import LogOut from './LogOut.js';
+import Header from './Header.js';
+import Contacts from './Contacts.js';
+import Login from './Login.js';
+import Home from './Home.js';
+import MyListings from './MyListings.js';
+import Profile from './Profile.js';
 
 function App() {
   const {loginWithRedirect, isAuthenticated} = useAuth0();
   return (
     <Router>
       <div className="App">
-      <header className="App-header">
-          <img src={house} className="App-logo" alt="logo" />
-          <p>
-              Welcome to HouseFinder.io! Login to our website to find the 
-              best house match for your needs.
-          </p>
-          {/* <Link to="/login">
-              <button className="btn btn-secondary">Log In</button>
-          </Link> */}
-          <Login />
-      </header>
+        <Header />
+        {/* <main className="App-main">
+            
+        </main> */}
         {/* Router for navigating to Login after clicking button*/}
         <Routes>
-              {/* <Route path = '/login' element={<Login />} /> */}
+              <Route path = '/login' element={<Login />} />
               <Route path = '/logout' element={<LogOut />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/mylistings" element={<MyListings />} />
+              <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>  
