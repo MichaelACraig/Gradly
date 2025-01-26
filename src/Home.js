f152cfa965d545cbeeffc7c9a97229947b4135f7
Sheng0841 <itsshengliu@gmail.com>
2025-01-25 20:44:01 -0500

    added back the search bar in home

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+  return (
+  <><div className="search">
+      <h1 className="homepage-intro-text">Welcome to Sublettee</h1>;
+      <input
+        type="text"
+        placeholder="Search..."
+        value={inputText}
+        onChange={inputHandler}/>
+
+    </div></>
+  );
   <br>
   </br>
   return <h1>Welcome to the Home Page</h1>;
