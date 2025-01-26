a351f686c3b3838c8f7d674cdde2a40f6cbe68c9
Sheng0841 <itsshengliu@gmail.com>
2025-01-25 21:47:32 -0500

    fixed underline

--- src/Login.js
+++ src/Login.js
@@ -1,29 +1,16 @@
 import React from "react";
 import LoginButton from "./LoginButton";
 import house from './house.jpg';
-import { useAuth0 } from "@auth0/auth0-react";
-import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const { isAuthenticated } = useAuth0();
-  const navigate = useNavigate();
-
-  React.useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/profile"); // Redirect to the profile page after login
-    }
-  }, [isAuthenticated, navigate]);
-
-  return (
-    <>
-      <img src={house} className="App-logo" alt="logo" />
-      <p>
+  return <>
+    <img src={house} className="App-logo" alt="logo" />
+    <p id="Welcome-message-login-page">
         Welcome to HouseFinder.io! Login to our website to find the 
         best house match for your needs.
-      </p>
-      <LoginButton />
-    </>
-  );
+    </p>
+    <LoginButton/>
+  </>;
 }
 
 export default Login;
